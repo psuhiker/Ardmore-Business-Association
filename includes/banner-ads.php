3dfa63e608d35ab4330393e690f57b8023e25480
Jordy Pickel <psuhiker@gmail.com>
2018-12-19 08:44:12 -0500

    Removal of Old Code

--- includes/banner-ads.php
+++ includes/banner-ads.php
@@ -1,35 +1,3 @@
-<!--<div class="ads aligncenter bannerslider">
-
-	<span class="ad">Sponsored</span>
-
-    <ul class="slides">
-
-		<?php query_posts(array('showposts', 'posts_per_page'=> '-1', 'post_type'=> 'businesses', 'meta_key' => 'aba_membership', 'meta_value' => '1', 'orderby' => 'rand', 'order' => 'ASC')); while (have_posts()) { the_post(); ?>
-		
-			<?php while ( have_rows('promotions') ) : the_row(); ?>
-			
-				<?php if( get_sub_field('banner_ad') ) { ?>
-				    <li data-track="ad-impression" data-ad="<?php the_sub_field('banner_ad'); ?>" data-link="<?php the_sub_field('link'); ?>">
-						<?php if( get_sub_field('link') ) { ?><a href="<?php the_sub_field('link'); ?>" target="_blank" data-track="ad-click"><?php } ?>
-							<img src="<?php the_sub_field('banner_ad'); ?>">
-						<?php if( get_sub_field('link') ) { ?></a><?php } ?>
-					</li>
-				<?php } ?>
-					
-			<?php endwhile; ?>
-			
-		<?php } ?><?php wp_reset_query(); ?>
-		
-		<li data-track="ad-impression" data-ad="<?php echo ( get_theme_mod( 'ad_banner' ) ); ?>" data-link="<?php echo ( get_theme_mod( 'ad_banner_url' ) ); ?>">
-			<a href="<?php echo ( get_theme_mod( 'ad_banner_url' ) ); ?>" target="_blank" data-track="ad-click">
-				<img src="<?php echo ( get_theme_mod( 'ad_banner' ) ); ?>">
-			</a>
-		</li>
-		
-	</ul>
-	
-</div>-->
-
 <section id="carousel" class="ads">
     <div id="block-ad-carousel" class="carousel fade" data-ride="carousel">
         
